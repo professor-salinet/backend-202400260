9aab94df8c24d5801768d15548a83ceed241e3a4
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-10-24 11:53:54 -0300

    Add files via upload

---
+++ OpenFile.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+public class OpenFile {
+    public static void abrirArquivo() {
+        try {
+            JFileChooser chooser = new JFileChooser();
+
+            chooser.setDialogTitle("Selecione o arquivo que deseja abrir");
+            chooser.setApproveButtonText("Abrir arquivo");
+            int returnVal1 = chooser.showOpenDialog(null);
+            String fileFullPath = "";
+            if (returnVal1 == JFileChooser.APPROVE_OPTION) {
+                fileFullPath = chooser.getSelectedFile().getAbsolutePath();
+            } else {
+                System.out.println("Que pena!");
+            }
+
+            try {
+                File file = new File(fileFullPath);
+                if(!Desktop.isDesktopSupported()) {
+                    System.err.println("not supported");
+                    return;
+                }
+                Desktop desktop = Desktop.getDesktop();
+                if(file.exists()) {
+                    desktop.open(file);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Arquivo aberto com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Não foi possível abrir o arquivo.");
+        }
+    }
+}
