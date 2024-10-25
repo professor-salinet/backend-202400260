87762801454fc602c27897f7105addc5deaef5b5
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-10-25 11:51:14 -0300

    Add files via upload

---
+++ DeleteFile.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.nio.file.*;
+
+public class DeleteFile {
+    public static void apagarArquivo() {
+        try {
+            JFileChooser chooser = new JFileChooser();
+
+            chooser.setDialogTitle("Selecione o arquivo que deseja apagar");
+            chooser.setApproveButtonText("Apagar arquivo");
+            int returnVal1 = chooser.showOpenDialog(null);
+            String fileFullPath = "";
+            if(returnVal1 == JFileChooser.APPROVE_OPTION) {
+                fileFullPath = chooser.getSelectedFile().getAbsolutePath();
+            } else {
+                System.out.println("Que pena!");
+            }
+
+            Path pathOrigin = Paths.get(fileFullPath);
+            Files.delete(pathOrigin);
+            System.out.println("Arquivo apagado com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Não foi possível apagar o arquivo.");
+        }
+    }
+}
